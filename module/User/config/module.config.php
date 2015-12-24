5b48006ef31cbed4f4683d8eabee839e7c4ed85a
Hung <hung-olivier.trinh@open-groupe.com>
2015-12-24 15:37:26 +0100

    add User module

---
+++ module/User/config/module.config.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+    'doctrine' => array(
+        'driver' => array(
+            'zfcuser_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => 'zfcuser_entity'
+                )
+            )
+        )
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'user' => __DIR__ . '/../view',
+            // replace zfcUser templates
+            'zfcuser' => __DIR__ . '/../view',
+        ),
+    ),
+);
